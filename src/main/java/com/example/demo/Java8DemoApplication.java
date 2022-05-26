fa7c182a8e566c98206b25c0ff79b2cb9cb0689d
prupertawraqpublishing <prupertawraqpublishing@gmail.com>
2022-05-26 11:31:31 +0400

    first commit

---
+++ src/main/java/com/example/demo/Java8DemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8DemoApplication.class, args);
+		
+		System.out.println("Testing");
+	}
+
+}
